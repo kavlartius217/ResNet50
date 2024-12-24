bf90d8606caa32296a575c868aad1471f3ec585b
kavlartius217 <kavlartius@gmail.com>
2024-12-24 13:02:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
 # Load the pre-trained model
-load_model("model.h5", compile=False)
+load_model("model.h5")
 
 # Define the class labels
 class_labels = ['COVID-19', 'NORMAL', 'VIRAL PNEUMONIA']
